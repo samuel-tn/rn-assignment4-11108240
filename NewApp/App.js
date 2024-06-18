3a5ef28cb543e8750a27f662895e600bde67f60e
samuel <samueltandoh4u@gmail.com>
2024-06-18 12:24:31 +0000

    new change

--- NewApp/App.js
+++ NewApp/App.js
@@ -1,21 +1,26 @@
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import loginscreen from './loginscreen';
+import homepagescreen from './homepagescreen';
+export default function App() { 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+    <SafeAreaView style={styles.container}>
+      <loginscreen/>
+      <homepagescreen/>
+    </SafeAreaView>
+    </ScrollView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F7F08E8',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
 });
